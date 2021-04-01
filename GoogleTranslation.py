55eaff106a6df9e3851c21532f006c4f601aedbb
AzureAD\AhmedShihab <shihab@premiumartsinc.com>
2021-04-01 19:32:43 +0900

    Google Translation and ReadWrite Excel Updated

--- GoogleTranslation.py
+++ GoogleTranslation.py
@@ -5,7 +5,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'GoogleTranslationAPIKey.json'
 
 def translate_text(text="YOUR_TEXT_TO_TRANSLATE", project_id="YOUR_PROJECT_ID"):
     """Translating Text."""
-
+    translatedText = ""
     client = translate.TranslationServiceClient()
 
     location = "global"
@@ -26,7 +26,39 @@ def translate_text(text="YOUR_TEXT_TO_TRANSLATE", project_id="YOUR_PROJECT_ID"):
 
     # Display the translation for each input text provided
     for translation in response.translations:
-        print("Translated text: {}".format(translation.translated_text))
-text = "Good morning. Goodbye. Welcome To MRTK"
-projectID="phrasal-bruin-309308"
-translate_text(text,projectID)
+        #print("Translated text: {}".format(translation.translated_text))
+        translatedText = format(translation.translated_text)
+    return translatedText
+
+def NewLineToStringList(string):
+    stringList = string.split("\n")
+    return stringList
+def JapaneseDotToStringList(string):
+    stringList = string.split("。")
+    return stringList
+def StringListToSingleLine(stringList):
+    stringLine = ""
+    for x in stringList:
+        stringLine= (stringLine+x+". ");
+    return stringLine
+def CombineEngAndJP(EnList,JpList):
+    for index in range(len(EnList)):
+        print(EnList[index])
+        print(JpList[index])
+text = "Topic. Sub Category. Sub Category 2. Sub Category 3. Sub Category 4. Content. URL"
+projectID = "phrasal-bruin-309308"
+
+
+
+
+text="""Overview of the eye tracking demo samples
+Setting up the MRTK eye tracking samples"""
+
+
+EnStringList = NewLineToStringList(text)
+EnStringLine = StringListToSingleLine(EnStringList)
+JpStringLine = translate_text(EnStringLine, projectID)
+JpStringList = JapaneseDotToStringList(JpStringLine)
+CombineEngAndJP(EnStringList, JpStringList)
+
+
--- ReadWriteExcel.py
+++ ReadWriteExcel.py
@@ -1,25 +1,41 @@
 import pandas as pd
 import xlsxwriter as xlWriter
+
 import random
 
-file = 'MRTK_Content.xlsx'
+file = 'MRTK UX Building Content.xlsx'
 xl = pd.read_excel(file, sheet_name="Sheet1", header=0)
+
+
+def trim(string):
+    string = string.replace(' ', '')
+    string = string.replace('\t', '')
+    string = string.replace('\r', '')
+    string = string.replace('\n', '')
+    return string
 
 
 def main():
     wb = xlWriter.Workbook("Apple.xlsx")
     ws = wb.add_worksheet("Sheet1")
-    xleys = xl.keys()
-    print(xlKeys[0])
+    cell_format = wb.add_format()
+    cell_format.set_text_wrap()
 
-    ws.write("A1", str(row[0]))
-    ws.write("B1", str(row[1]))
-    ws.write("C1", str(row[2]))
-    ws.write("D1", str(row[3]))
-    ws.write("E1", str(row[4]))
-    ws.write("F1", str(row[5]))
-    ws.write("G1", str(row[6]))
+    xlKeys = xl.keys()
+    for key in xlKeys:
+        if key == "":
+            continue
+        else:
+            print(key)
 
+    """
+    ws.write("A1", str(xlKeys[0]), cell_format)
+    ws.write("B1", str(xlKeys[1]), cell_format)
+    ws.write("C1", str(xlKeys[2]), cell_format)
+    ws.write("D1", str(xlKeys[3]), cell_format)
+    ws.write("E1", str(xlKeys[4]), cell_format)
+    ws.write("F1", str(xlKeys[5]), cell_format)
+    """
     for index, row in xl.iterrows():
         """
         keyTopic = row["Topic\nトピック"]
@@ -47,8 +63,9 @@ def main():
         ws.write("F" + str(index + 1), str(row[5]))
         ws.write("G" + str(index + 1), str(row[6]))
         """
-        #print(row[0])
+        # print(row[0])
     wb.close()
+
 
 main()
 """
